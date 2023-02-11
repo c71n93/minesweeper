cbae1900de0b74621772ff0f4f21f31d82a97c29
c71n93 <korostinskii.r@phystech.edu>
2023-02-11 23:45:00 +0300

    finished Stage 2/5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,18 @@
 package minesweeper
 
+import java.util.Scanner
+import kotlin.random.Random
+
+const val FIELD_SIZE = 9
 fun main() {
-    val fieldSize = 10
-    val field = MutableList(fieldSize) { MutableList(fieldSize) { '.' } }
-    for (i in 0..field.lastIndex) {
-        field[i][i] = 'x'
+    val field = MutableList(FIELD_SIZE) { MutableList(FIELD_SIZE) { '.' } }
+    val numOfMines = requestNumberOfMines()
+    val minesCoordinates = getMineCoordinates(numOfMines)
+
+    for (i in 0 .. minesCoordinates.lastIndex) {
+        val x = minesCoordinates[i] / FIELD_SIZE
+        val y = minesCoordinates[i] % FIELD_SIZE
+        field[x][y] = 'X'
     }
 
     for (i in 0..field.lastIndex) {
@@ -13,4 +21,25 @@ fun main() {
         }
         println()
     }
+}
+
+fun getMineCoordinates(numOfMines: Int): MutableList<Int> {
+    val minesCoordinates = MutableList(numOfMines) { -1 }
+    for (i in 0 until numOfMines) {
+        var randomCoordinate: Int
+        /* TODO: this is very stupid decision, because on the last iteration
+             there is a small chance to get suitable random number, so this
+             loop will make a lot of iterations until it got suitable number */
+        do {
+            randomCoordinate = Random.nextInt(0, FIELD_SIZE * FIELD_SIZE)
+        } while (randomCoordinate in minesCoordinates)
+        minesCoordinates[i] = randomCoordinate
+    }
+    return minesCoordinates
+}
+
+fun requestNumberOfMines(): Int {
+    print("How many mines do you want on the field?")
+    val scanner = Scanner(System.`in`)
+    return scanner.nextInt()
 }
