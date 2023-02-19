3eef7cfe86b81c6f0f2e526d6ff92da6047d0f18
c71n93 <korostinskii.r@phystech.edu>
2023-02-19 21:00:20 +0300

    Stage 4: implement marking cells

--- src/main/kotlin/minesweeper/field/Field.kt
+++ src/main/kotlin/minesweeper/field/Field.kt
@@ -2,18 +2,21 @@ package minesweeper.field
 
 import kotlin.random.Random
 
-private const val SAFE_CELL = '.'
-private const val MINE_CELL = 'X'
+const val CLOSED_CELL = '.'
+const val MINE_CELL = 'X'
+const val MARKED_CELL = '*'
 
-class Field(private val height: Int, private val width: Int) {
-    private val field = MutableList(height) { MutableList(width) { SAFE_CELL } }
+data class Cell(var content: Char = CLOSED_CELL, var isMarked: Boolean = false)
+
+class Field(private val _height: Int, private val _width: Int) {
+    private val _field = MutableList(_height) { MutableList(_width) { Cell() } }
 
     // took this solution from here: https://hyperskill.org/projects/8/stages/47/implement#solutions-309746
     fun generateField(numOfMines: Int) {
         var restMines = numOfMines
-        var restCells = height * width
-        for (i in 0 .. field.lastIndex) {
-            for (j in 0 .. field[i].lastIndex) {
+        var restCells = _height * _width
+        for (i in 0.._field.lastIndex) {
+            for (j in 0.._field[i].lastIndex) {
                 if (Random.nextInt(0, restCells) < restMines) {
                     placeMine(i, j)
                     restMines--
@@ -22,26 +25,75 @@ class Field(private val height: Int, private val width: Int) {
             }
         }
     }
-    fun printField() {
-        for (i in 0..field.lastIndex) {
-            for (j in field[i]) {
-                print(j.toString())
+
+    fun printOpenField() {
+        printColumnCoordinates()
+        printHorizontalDelimiters()
+        for (i in 0.._field.lastIndex) {
+            print("$i|")
+            for (cell in _field[i]) {
+                print(cell.content.toString())
             }
-            println()
+            println("|")
         }
+        printHorizontalDelimiters()
+    }
+
+    fun printCloseField() {
+        printColumnCoordinates()
+        printHorizontalDelimiters()
+        for (i in 0.._field.lastIndex) {
+            print("${i + 1}|")
+            for (cell in _field[i]) {
+                if (cell.content in '1'..'7') {
+                    print(cell.content.toString())
+                } else {
+                    if (cell.isMarked) {
+                        print(MARKED_CELL.toString())
+                    } else {
+                        print(CLOSED_CELL.toString())
+                    }
+                }
+            }
+            println("|")
+        }
+        printHorizontalDelimiters()
+    }
+    fun setOrDeleteMark(row: Int, col: Int) = if (_field[row][col].content in '1'..'7') { //todo: validate row and col
+        println("There is a number here!")
+        false
+    } else {
+        _field[row][col].isMarked = true
+        true
     }
 
     // took this solution from here: https://hyperskill.org/projects/8/stages/48/implement#solutions-190159
-    private fun placeMine(row: Int, column: Int) {
-        field[row][column] = MINE_CELL
-        for (r in maxOf(row - 1, 0)..minOf(row + 1, height - 1)) {
-            for (c in maxOf(column - 1, 0)..minOf(column + 1, width - 1)) {
-                when (field[r][c]) {
-                    SAFE_CELL -> field[r][c] = '1'
-                    in '1'..'7' -> field[r][c]++
+    private fun placeMine(row: Int, col: Int) {
+        _field[row][col].content = MINE_CELL
+        for (r in maxOf(row - 1, 0)..minOf(row + 1, _height - 1)) {
+            for (c in maxOf(col - 1, 0)..minOf(col + 1, _width - 1)) {
+                when (_field[r][c].content) {
+                    CLOSED_CELL -> _field[r][c].content = '1'
+                    in '1'..'7' -> _field[r][c].content++
                 }
 
             }
         }
+    }
+
+    private fun printColumnCoordinates() {
+        print(" |")
+        for (i in 1.._width) {
+            print(i)
+        }
+        println("|")
+    }
+
+    private fun printHorizontalDelimiters() {
+        print("-|")
+        for (i in 1.._width) {
+            print("-")
+        }
+        println("|")
     }
 }
--- src/main/kotlin/minesweeper/launch/Main.kt
+++ src/main/kotlin/minesweeper/launch/Main.kt
@@ -9,11 +9,26 @@ fun main() {
     val field = Field(FIELD_HEIGHT, FIELD_WIDTH)
     val numOfMines = requestNumberOfMines()
     field.generateField(numOfMines)
-    field.printField()
+    field.printCloseField()
+    while (true) {
+        requestSetOrDeleteMark(field)
+        field.printCloseField()
+    }
 }
 
 fun requestNumberOfMines(): Int {
     print("How many mines do you want on the field? ")
     val scanner = Scanner(System.`in`)
     return scanner.nextInt()
+}
+
+fun requestSetOrDeleteMark(field: Field) {
+    print("Set/delete mines marks (x and y coordinates): ")
+    val scanner = Scanner(System.`in`)
+    var isMarkSetOrDeleted = false
+    while (!isMarkSetOrDeleted) {
+        val column = scanner.nextInt()
+        val row = scanner.nextInt()
+        isMarkSetOrDeleted = field.setOrDeleteMark(row - 1, column - 1)
+    }
 }
